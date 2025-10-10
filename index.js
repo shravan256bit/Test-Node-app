3890787fd9950806b21a1d3d5081d12dcafda85b
whatsnext <whatsnext@localhost.localdomain>
2025-10-10 13:26:42 +0530

    Node app

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello from AWS Deployment Demo!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
